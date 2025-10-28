c465327c1370575328172289226332bcfbd796dd
rivollierarnaud-netizen <rivollierarnaud@gmail.com>
2025-10-28 18:58:37 +0100

    front2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('whatyearisit-backend-opal-eight.vercel.app')
+fetch('whatyearisit-backend-opal-eight.vercel.apphttps://whatyearisit-backend-opal-eight.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.year;
